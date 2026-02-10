b1003af60744d1b950588d895483e1611a233a10
Saee1410 <saivijaybhosale@gmail.com>
2026-02-10 17:16:50 +0530

    improve all code

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["https://housefull-frontend.vercel.app", "http://localhost:3000"],
+  origin: ["https://house-full-frontend.vercel.app", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
